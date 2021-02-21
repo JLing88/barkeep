04ed07af6802459f2dd33c350e1514a44bf834f7
Jesse Ling <jesse.ryan.ling@gmail.com>
2021-02-21 16:00:51 -0700

    add spec for #show and add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   apipie
   namespace :api do
     namespace :v1 do
-      resources :searches, only: [:index]
+      resources :searches, only: [:index, :show]
     end
   end
 end
--- spec/requests/api/v1/searches_request_spec.rb
+++ spec/requests/api/v1/searches_request_spec.rb
@@ -10,14 +10,30 @@ RSpec.describe "Api::V1::Searches", type: :request do
       results = JSON.parse(response.body)
 
       expect(response.status).to eq(200)
-      expect(results).to have_key("data")
-      expect(results["data"].count).to eq(2)
-      expect(results["data"][0]["attributes"]["id"]).to eq(search_2.id)
-      expect(results["data"][0]["attributes"]["query"]).to eq(search_2.query)
-      expect(results["data"][0]["attributes"]["url"]).to eq(search_2.url)
-      expect(results["data"][1]["attributes"]["id"]).to eq(search_1.id)
-      expect(results["data"][1]["attributes"]["query"]).to eq(search_1.query)
-      expect(results["data"][1]["attributes"]["url"]).to eq(search_1.url)
+      expect(results).to have_key('data')
+      expect(results['data'].count).to eq(2)
+      expect(results['data'][0]['attributes']['id']).to eq(search_2.id)
+      expect(results['data'][0]['attributes']['query']).to eq(search_2.query)
+      expect(results['data'][0]['attributes']['url']).to eq(search_2.url)
+      expect(results['data'][1]['attributes']['id']).to eq(search_1.id)
+      expect(results['data'][1]['attributes']['query']).to eq(search_1.query)
+      expect(results['data'][1]['attributes']['url']).to eq(search_1.url)
+    end
+  end
+
+  describe '#show' do
+    it 'returns the Search object by id' do
+      search_1 = create(:search)
+      search_2 = Search.create!(query: 'mojito', url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito')
+
+      get "/api/v1/searches/#{search_1.id}"
+      results = JSON.parse(response.body)
+
+      expect(response.status).to eq(200)
+      expect(results['data']).count to eq(1)
+      expect(results['data'][0]['attributes']['id']).to eq(search_1.id)
+      expect(results['data'][0]['attributes']['query']).to eq(search_1.query)
+      expect(results['data'][0]['attributes']['url']).to eq(search_1.url)
     end
   end
 end
