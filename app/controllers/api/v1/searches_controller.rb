d044182fc0af32e5dd50f3de0a3b7fcfdbf0c177
Jesse Ling <jesse.ryan.ling@gmail.com>
2021-02-22 16:08:41 -0700

    create search_controller#delete

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -2,17 +2,26 @@ class Api::V1::SearchesController < ApplicationController
   api :GET, '/v1/searches'
   def index
     searches = Search.all.order(updated_at: :desc)
-
     results = SearchSerializer.new(searches).serializable_hash
+
     render json: results, status: 200
   end
 
   api :GET, '/v1/searches/:id'
-  param :id, :number, required: true, desc: 'id of the requested search'
+  param :id, :number, required: true, desc: 'id of the requested search object'
   def show
     search = Search.find(params[:id])
-
     result = SearchSerializer.new(search).serializable_hash
+
     render json: result, status: 200
+  end
+
+  api :DELETE, '/v1/searches/:id'
+  param :id, :number, required: true, desc: 'id of the search object to be deleted'
+  def destroy
+    search = Search.find(params[:id])
+    search.destroy
+
+    render json: "Search has been deleted", status: 200
   end
 end
