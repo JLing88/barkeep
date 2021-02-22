f88c3613bef7648e0358c8c7dccd22d4b62f2f82
Jesse Ling <jesse.ryan.ling@gmail.com>
2021-02-21 19:53:06 -0700

    create search_controller#show

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::SearchesController < ApplicationController
     results = SearchSerializer.new(searches).serializable_hash
     render json: results, status: 200
   end
+
+  api :GET, '/v1/searches/:id'
+  param :id, :number, required: true, desc: 'id of the requested search'
+  def show
+    search = Search.find(params[:id])
+
+    result = SearchSerializer.new(search).serializable_hash
+    render json: result, status: 200
+    
+  end
 end
